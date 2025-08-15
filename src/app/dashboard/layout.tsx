5118208ee4ab7e1880405bdff452123f51c7d9ba
Kashif <kashif.ahmad@ccript.com>
2025-08-15 18:37:18 +0500

    c

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,9 +14,9 @@ export default async function DashboardLayout({
 }) {
   return (
     <KBar>
-      <div className='bg-background flex min-h-screen w-full flex-col p-6 '>
+      <div className='bg-background flex min-h-screen w-full flex-col p-6'>
         <Header />
-        {/* <main className='flex-1'>{children}</main> */}
+        <main className='flex-1'>{children}</main>
       </div>
     </KBar>
   );
