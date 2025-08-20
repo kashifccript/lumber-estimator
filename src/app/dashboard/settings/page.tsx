769f7806db9489a74fe159bcc3dc7f8e343de3f5
kashif <kashif.ahmad@ccript.com>
2025-08-21 01:25:34 +0500

    setting page added

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Settings'
+};
+
+export default function page() {
+  return (
+    <div className='flex w-full flex-col p-4'>
+      <h1 className='text-2xl font-bold'>Settings</h1>
+    </div>
+  );
+}
