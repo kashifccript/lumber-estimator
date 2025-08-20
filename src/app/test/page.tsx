a95bd2f4250cddf27aa407a5e10e8babf48715ca
kashif <kashif.ahmad@ccript.com>
2025-08-20 15:34:25 +0500

    Removed test page

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import EstimationDetailsViewPage from '@/features/estimation-details/components/estimation-details-view-page';
-
-export const metadata = {
-  title: 'Estimation Details'
-};
-
-export default function page() {
-  return <EstimationDetailsViewPage />;
-}
