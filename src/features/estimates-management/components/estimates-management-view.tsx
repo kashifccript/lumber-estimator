8144708ec6d1a378355bdfa8623ca7d3f7adbee5
Kashif <kashif.ahmad@ccript.com>
2025-09-11 19:06:36 +0500

    Update estimates-management-view.tsx

--- src/features/estimates-management/components/estimates-management-view.tsx
+++ src/features/estimates-management/components/estimates-management-view.tsx
@@ -118,7 +118,7 @@ export default function EstimatesManagementViewPage() {
               <Search className='absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-[#292D32]' />
               <Input
                 type='text'
-                placeholder='Search by project name, description, or estimator'
+                placeholder='Search Estimator'
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 className='h-[48px] rounded-[8px] border border-[#8896AB33] py-2 pr-4 pl-10 placeholder:text-[#292D32] focus-visible:ring-0 focus-visible:ring-offset-0'
