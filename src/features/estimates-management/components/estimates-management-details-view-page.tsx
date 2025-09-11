7022d769d23355c13ea2a24055ebed08bf3b7874
Kashif <kashif.ahmad@ccript.com>
2025-09-11 19:20:59 +0500

    Update estimates-management-details-view-page.tsx

--- src/features/estimates-management/components/estimates-management-details-view-page.tsx
+++ src/features/estimates-management/components/estimates-management-details-view-page.tsx
@@ -107,17 +107,17 @@ export default function EstimatesManagementDetails() {
                 }
               ]}
             />
-            <div className='relative w-full max-w-sm'>
-              {/* Search Icon */}
+            {/* <div className='relative w-full max-w-sm'>
+             
               <Search className='absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-[#292D32]' />
 
-              {/* Input */}
+           
               <Input
                 type='text'
                 placeholder='Search'
                 className='h-[48px] rounded-[8px] border border-[#8896AB33] py-2 pr-4 pl-10 placeholder:text-[#292D32] focus-visible:ring-0 focus-visible:ring-offset-0'
               />
-            </div>
+            </div> */}
           </div>
           {/* Profile Section */}
           <div className='flex items-center justify-between gap-3 rounded-sm bg-white px-10 py-4'>
