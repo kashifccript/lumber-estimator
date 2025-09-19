c8fe9dba3eea41211d39e1e861936c4485eb5ab5
kashif <kashif.ahmad@ccript.com>
2025-09-19 21:41:52 +0500

    changes to signup

--- src/components/auth/auth-heading.tsx
+++ src/components/auth/auth-heading.tsx
@@ -14,7 +14,7 @@ export default function AuthHeading({
 }: AuthHeadingProps) {
   return (
     <div className='text-left'>
-      <h1 className='text-[32px] leading-[100%] font-semibold text-[1F1F1F] sm:text-[36px] md:text-[42px]'>
+      <h1 className='leading-[100%] font-semibold text-secondary text-[36px] md:text-[42px]'>
         {title}
         {highlight && <span className='text-primary'>{highlight}</span>}
       </h1>
