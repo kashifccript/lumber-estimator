c16868a4f50b032bfe33439088d726f4446cd641
kashif <kashif.ahmad@ccript.com>
2025-09-24 22:27:38 +0500

    c

--- src/components/modal/alert-modal.tsx
+++ src/components/modal/alert-modal.tsx
@@ -13,7 +13,7 @@ interface AlertModalProps {
   confirmText?: string;
   cancelText?: string;
   variant?: 'approve' | 'reject' | 'default';
-  isEstimate?:boolean
+  isEstimate?: boolean;
 }
 
 export const AlertModal: React.FC<AlertModalProps> = ({
@@ -44,60 +44,60 @@ export const AlertModal: React.FC<AlertModalProps> = ({
       description={description}
       isOpen={isOpen}
       onClose={onClose}
-    >{isEstimate ?
-          <div className='flex items-center flex-col md:flex-row justify-end space-x-2 pt-6 px-1 gap-4'>
-        <Button
-          disabled={loading}
-          variant={'reject'}
-          onClick={onClose}
-          className='w-full md:w-auto'
-        >
-          {cancelText}
-
-        </Button>
-        <Button
-          disabled={loading}
-          // size={'md'}
-          variant={
-            variant === 'approve'
-              ? 'approve'
-              : variant === 'reject'
-                ? 'reject'
-                : 'primary'
-          }
-          className={`w-full md:w-auto ${variant === 'reject' ? 'bg-[#C81E1E] text-white hover:bg-[#C81E1E]/90' : ''}`}
-          onClick={onConfirm}
-        >
-          {confirmText}
-        </Button>
-      </div>:
-      <div className='flex items-center justify-end space-x-2 pt-6 px-8'>
-        <Button
-          disabled={loading}
-          variant={'reject'}
-          onClick={onClose}
-          className='w-auto'
-        >
-          {cancelText}
-
-        </Button>
-        <Button
-          disabled={loading}
-          // size={'md'}
-          variant={
-            variant === 'approve'
-              ? 'approve'
-              : variant === 'reject'
-                ? 'reject'
-                : 'primary'
-          }
-          className={`w-auto ${variant === 'reject' ? 'bg-[#C81E1E] text-white hover:bg-[#C81E1E]/90' : ''}`}
-          onClick={onConfirm}
-        >
-          {confirmText}
-        </Button>
-      </div>
-}
+    >
+      {isEstimate ? (
+        <div className='flex flex-col items-center justify-end gap-1.5 space-x-2 px-1 pt-6 md:flex-row md:gap-4'>
+          <Button
+            disabled={loading}
+            variant={'reject'}
+            onClick={onClose}
+            className='w-full md:w-auto'
+          >
+            {cancelText}
+          </Button>
+          <Button
+            disabled={loading}
+            // size={'md'}
+            variant={
+              variant === 'approve'
+                ? 'approve'
+                : variant === 'reject'
+                  ? 'reject'
+                  : 'primary'
+            }
+            className={`w-full md:w-auto ${variant === 'reject' ? 'bg-[#C81E1E] text-white hover:bg-[#C81E1E]/90' : ''}`}
+            onClick={onConfirm}
+          >
+            {confirmText}
+          </Button>
+        </div>
+      ) : (
+        <div className='flex items-center justify-end space-x-2 px-8 pt-6'>
+          <Button
+            disabled={loading}
+            variant={'reject'}
+            onClick={onClose}
+            className='w-auto'
+          >
+            {cancelText}
+          </Button>
+          <Button
+            disabled={loading}
+            // size={'md'}
+            variant={
+              variant === 'approve'
+                ? 'approve'
+                : variant === 'reject'
+                  ? 'reject'
+                  : 'primary'
+            }
+            className={`w-auto ${variant === 'reject' ? 'bg-[#C81E1E] text-white hover:bg-[#C81E1E]/90' : ''}`}
+            onClick={onConfirm}
+          >
+            {confirmText}
+          </Button>
+        </div>
+      )}
     </Modal>
   );
 };
